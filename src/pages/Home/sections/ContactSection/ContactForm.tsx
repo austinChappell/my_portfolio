fc9744c5e49df6eaf9d2731cfed0b37695591a0b
Austin <austin.w.chappell@gmail.com>
2020-01-13 00:23:41 -0600

    remove action

--- src/pages/Home/sections/ContactSection/ContactForm.tsx
+++ src/pages/Home/sections/ContactSection/ContactForm.tsx
@@ -50,7 +50,6 @@ const ContactForm: React.FC = () => {
     <Wrapper>
       <Card>
         <Form
-          action="/contact"
           data-netlify-honeypot="bot-field"
           data-netlify="true"
           method="post"
